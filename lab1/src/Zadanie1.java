130e2e3dc6a7ea6f3cca0e2beb0919d8ac1e694e
mich9874 <73462339+mich9874@users.noreply.github.com>
2021-10-15 00:44:22 +0200

    Add files via upload

---
+++ lab1/src/Zadanie1.java
@@ -0,0 +1,74 @@
+public class Zadanie1 {
+    public static void main (String[] args) {
+        System.out.println(31 + 29 + 31);
+        //zadnie2
+        System.out.println(1+2+3+4+5+6+7+8+9+10);
+        //zadanie3
+        System.out.println(2 * 3 * 4 * 5 * 6 * 7 * 8 * 9 * 10);
+        //zadanie4
+        int x=1000;
+        double a=x*1.06;
+        double b=a*1.06;
+        double c=b*1.06;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        //zadanie5
+        System.out.println("+----+");
+        System.out.println("|Java|");
+        System.out.println("+----+");
+        //zadanie6
+        System.out.println("  /////");
+        System.out.println(" +\"\"\"\"+");
+        System.out.println("( |°°| )");
+        System.out.println(" | ^  |");
+        System.out.println("( `-´ )");
+        System.out.println("+------+");
+        //zadanie 10
+        System.out.println("Szybcy i wściekli 1");
+        System.out.println("Szybcy i wściekli 5");
+        System.out.println("Szybcy i wściekli Tokyo Drift");
+        //zadanie11
+        System.out.println("Dwadzieścia siedem kości,\n" +
+                "trzydzieści pięć mięśni,\n" +
+                "około dwóch tysięcy komórek nerwowych\n" +
+                "w każdej opuszce naszych pięciu palców.\n" +
+                "To zupełnie wystarczy,\n" +
+                "żeby napisać „Mein Kampf”\n" +
+                "albo „Chatkę Puchatka”.");
+        System.out.println("Godzina z nocy na dzień.\n" +
+                "\n" +
+                "Godzina z boku na bok.\n" +
+                "\n" +
+                "Godzina dla trzydziestoletnich.\n" +
+                "\n" +
+                " \n" +
+                "\n" +
+                "Godzina uprzątnięta pod kogutów pianie.\n" +
+                "\n" +
+                "Godzina, kiedy ziemia zapiera nas.\n" +
+                "\n" +
+                "Godzina, kiedy wieje od wygasłych gwiazd.\n" +
+                "\n" +
+                "Godzina a-czy-po-nas-nic-nie-pozostanie.\n" +
+                "\n" +
+                " \n" +
+                "\n" +
+                "Godzina pusta.\n" +
+                "\n" +
+                "Głucha, czcza.\n" +
+                "\n" +
+                "Dno wszystkich innych godzin.\n" +
+                "\n" +
+                "  \n" +
+                "\n" +
+                "Nikomu nie jest dobrze o czwartej nad ranem.\n" +
+                "\n" +
+                "Jeśli mrówkom jest dobrze o czwartej nad ranem\n" +
+                "\n" +
+                "- pogratulujmy mrówkom. I niech przyjdzie piąta,\n" +
+                "\n" +
+                "o ile mamy dalej żyć.");
+    }
+}
+
